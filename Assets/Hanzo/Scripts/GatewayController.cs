ef7e6a0b9c682adf8981d8335971f3291424cf54
Tamar Minimah <mtminimah@griotstudios.org>
2023-02-21 13:27:16 +0100

    Gateway Controller Update

---
+++ Assets/Hanzo/Scripts/GatewayController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Hanzo
+{
+    public class GatewayController : MonoBehaviour
+    {
+
+        public int gateValue;
+
+        public TMPro.TextMeshProUGUI gateText;
+
+        bool isGatePassed = false;
+        // SpawnController spawnController;
+        public GameObject PlayerPrefabSpawner;
+
+
+
+        void Awake()
+        {
+            PlayerPrefabSpawner = GameObject.Find("PlayerSpawner");
+        }
+
+       
+        void Start()
+        {
+            AddGateValue();
+        }
+
+        void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag("Player"))
+            {
+                isGatePassed = true;
+                SpawnController.Instance.SpawnPlayer(3);
+                Destroy(this.gameObject);
+            }
+        }
+
+        void AddGateValue()
+        {
+            gateText.text = gateValue.ToString();
+        }
+
+    }
+}
+
--- Assets/Hanzo/Scripts/SpawnController.cs
+++ Assets/Hanzo/Scripts/SpawnController.cs
@@ -6,16 +6,33 @@ namespace Hanzo
 {
     public class SpawnController : MonoBehaviour
     {
+        public static SpawnController Instance;
+
+
         public float speed = 5f;
         public float hSpeed;
         float maxXposition = 4.10f;
         public GameObject PlayerPrefab;
 
+        [SerializeField]
+        bool isPlayerMoving = true;
 
+        void Awake()
+        {
+            if (Instance != null && Instance != this)
+            {
+                Destroy(this);
+            }
+            else
+            {
+                Instance = this;
+            }
+        }
         // Start is called before the first frame update
         void Start()
         {
-             SpawnPlayer(3);
+            // SpawnPlayer(3);
+
         }
 
         // Update is called once per frame
@@ -23,7 +40,12 @@ namespace Hanzo
         {
             float touchX = 0;
             float newXValue = 0;
-            
+
+            if (isPlayerMoving == false)
+            {
+                return;
+            }
+
             if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved)
             {
                 hSpeed = 250f;
@@ -37,26 +59,36 @@ namespace Hanzo
             }
 
             newXValue = transform.position.x + hSpeed * touchX * Time.deltaTime;
-            newXValue = Mathf.Clamp(newXValue,-maxXposition, maxXposition);
+            newXValue = Mathf.Clamp(newXValue, -maxXposition, maxXposition);
             Vector3 newPos = new Vector3(newXValue, transform.position.y, transform.position.z + Time.deltaTime * speed);
             transform.position = newPos;
         }
 
-         public void SpawnPlayer(int playerCount)
+        public void SpawnPlayer(int playerCount)
         {
             //Spawn Players inside Player Child
             for (int i = 0; i < playerCount; i++)
             {
-                 Instantiate(PlayerPrefab, PlayerPosition(), Quaternion.identity, transform);
+                Instantiate(PlayerPrefab, PlayerPosition(), Quaternion.identity, transform);
             }
-           
+
         }
+
 
         public Vector3 PlayerPosition()
         {
             Vector3 position = Random.insideUnitSphere * 0.1f;
             Vector3 newPos = transform.position + position;
+            newPos.y *= 0f;
             return newPos;
+        }
+
+        void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag("Finish Line"))
+            {
+                isPlayerMoving = false;
+            }
         }
     }
 
