58b1669bf4b9b52cc9ec6053213b297329d2f2eb
tamar <westtmu@gmail.com>
2023-03-08 19:53:23 +0100

    Enemy and Player Facing Each Other

--- Assets/Hanzo/Scripts/MobSpawnerController.cs
+++ Assets/Hanzo/Scripts/MobSpawnerController.cs
@@ -8,6 +8,17 @@ namespace Hanzo
     {
         public GameObject mob;
         public List<GameObject> mobs = new List<GameObject>();
+
+        SpawnController spawnController;
+        GameObject playerSpawnerGo;
+
+
+        private void Awake()
+        {
+            playerSpawnerGo = GameObject.FindGameObjectWithTag("PlayerSpawner");
+            spawnController = playerSpawnerGo.GetComponent<SpawnController>();
+        }
+
         // Start is called before the first frame update
         void Start()
         {
@@ -20,17 +31,42 @@ namespace Hanzo
 
         }
 
-        public void SpawnMob(int moobCount)
+        public void SpawnMob(int mobCount)
         {
-            GameObject mobGO = Instantiate(mob, GetMobPosition(), Quaternion.identity);
-            mobs.Add(mobGO);
+            for (int i = 0; i < mobCount; i++)
+            {
+                Quaternion mobRot = Quaternion.Euler(new Vector3(0, 180, 0));
+                GameObject mobGO = Instantiate(mob, GetMobPosition(), mobRot, transform);
+                mobs.Add(mobGO);
+            }
+
         }
 
         public Vector3 GetMobPosition()
         {
             Vector3 pos = Random.insideUnitSphere * 0.1f;
             Vector3 newPos = transform.position + pos;
-            return newPos; 
+            return newPos;
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag("Player"))
+            {
+                GetComponent<BoxCollider>().enabled = false;
+                spawnController.MobDetected(other.gameObject);
+                LookAtPlayer(other.gameObject);
+            }
+        }
+
+        private void LookAtPlayer(GameObject target)
+        {
+            Vector3 dir = transform.position - target.transform.position;
+            Quaternion lookRot = Quaternion.LookRotation(dir);
+            lookRot.x = 0;
+            lookRot.z = 0;
+
+            transform.rotation = lookRot;
         }
 
 
--- Assets/Hanzo/Scripts/SpawnController.cs
+++ Assets/Hanzo/Scripts/SpawnController.cs
@@ -110,9 +110,27 @@ namespace Hanzo
 
         public void CopGotKilled(GameObject copGO)
         {
-                playerList.Remove(copGO);
-                Destroy(copGO);
+            playerList.Remove(copGO);
+            Destroy(copGO);
         }
+
+        public void MobDetected(GameObject target)
+        {
+            isPlayerMoving = !true;
+            LookAtEnemy(target);
+        }
+
+        private void LookAtEnemy(GameObject target)
+        {
+            // Vector3 dir = target.transform.position - transform.position;
+            Vector3 dir = transform.position - target.transform.position ;
+            Quaternion lookRotation = Quaternion.LookRotation(dir);
+            lookRotation.x = 0;
+            lookRotation.z = 0;
+            transform.rotation =lookRotation;
+        }
+
+
     }
 }
 
